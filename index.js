948aeefc8e39d4c918c6da070d397fcffc220f60
Erich <eheiseler@gmail.com>
2022-01-22 23:22:11 -0500

    commit tree a573332186aa8eb52d4ba8b158183a3dfcf1c61d

---
+++ index.js
@@ -0,0 +1,51 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a , b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    n++;
+    return n;
+}
+
+function decrement(n) {
+    n--;
+    return n;
+}
+
+function makeInt(string) {
+    let parsedInt = parseInt(string, 10);
+    return parsedInt;
+}
+
+function preserveDecimal(string) {
+    let parsedFloat = parseFloat(string);
+    return parsedFloat;
+}
